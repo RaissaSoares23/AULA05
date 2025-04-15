76645a9ae71d6d31386875d20f9e7d8d43285828
raissa soares <raissasoaresandradedasilva@gmail.com>
2025-04-15 16:55:28 -0300

    atualização do código

--- exercicio07.py
+++ exercicio07.py
@@ -1,9 +1,9 @@
-nota1 = int (input("Digite o valor um:"))
-nota2= int(input("Digite o valor dois:"))
+nota1 = float (input("Digite o valor um:"))
+while nota1 <0 or nota1>10:
+    nota1 = float(input("Nota inválida, digite novamente:"))
+nota2= float(input("Digite o valor dois:"))
+while nota2 <0 or nota2>10:
+    nota2 = float(input("Nota inválida, digite novamente:"))
 
-while nota1 <0:
-    print("Nota inválida, digite novamente:")
-
-
-media=nota1/nota2
+media=(nota1+nota2)/2
 print (media)
---
+++ exercicio08.py
@@ -0,0 +1,13 @@
+resp = "s"
+while resp =="s":
+    nota1 = float (input("Digite o valor um:"))
+    while nota1 <0 or nota1>10:
+        nota1 = float(input("Nota inválida, digite novamente:"))
+    nota2= float(input("Digite o valor dois:"))
+    while nota2 <0 or nota2>10:
+        nota2 = float(input("Nota inválida, digite novamente:"))
+
+    media=(nota1+nota2)/2
+    print (media)
+
+    resp= input("Deseja fazer o calculo novamente (s/n")
